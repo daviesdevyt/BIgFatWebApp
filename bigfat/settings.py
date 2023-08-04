96b4e8bfecd3d005ed7a18cff8bb0216a2902143
Davies Dev <davidonzan@gmail.com>
2023-08-04 13:21:42 +0100

    Finishing for deploy

--- bigfat/settings.py
+++ bigfat/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'bigfat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'), 
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
